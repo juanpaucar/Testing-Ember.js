14a90556d5c527320d9486ee1a0f8a71e7ea67f5
Juan Paucar <juantotish1@hotmail.com>
2014-07-23 18:14:56 -0500

    WIP

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 window.Todos = Ember.Application.create();
 
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
+
 Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    // additional child routes
+    this.route('active');
+    this.route('completed');
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
@@ -15,5 +23,27 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
